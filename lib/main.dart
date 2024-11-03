f7b885a27aa5cc91d736b3f2985889bd3669dd0d
xicc1005 <lanabaserrol@yahoo.com>
2024-11-04 05:18:34 +0800

    added google-services.json and main.dart firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'view/googleBottomBar.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    // Initialize Firebase for all platforms
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBd56YvEheA6dF_144vHsCXLuVNtVf087M',
+        appId: '1:459147193945:android:8c2ff1b6516fddb71a4b48',
+        messagingSenderId: '459147193945',
+        projectId: 'nemsync-syncite',
+        storageBucket: 'gs://mike-test-app-34a08.appspot.com',
+      ),
+    );
+  } catch (e) {
+    print('Firebase initialization ffailed: $e');
+  }
+
   runApp(MyApp());
 }
 
